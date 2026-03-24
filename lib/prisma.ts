cc5b980c748fbc1ed85c4e740380123a093313f5
Gandalfelsavia <saviafabio@gmail.com>
2026-03-24 15:19:42 +0100

    fix deploy prisma pulito

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -4,19 +4,8 @@ const globalForPrisma = globalThis as unknown as {
   prisma: PrismaClient | undefined;
 };
 
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
-
-const adapter = new PrismaPg(pool);
-
 export const prisma =
   globalForPrisma.prisma ??
-  new PrismaClient({
-    adapter,
-  });
+  new PrismaClient();
 
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
